e04cef3a28e19a7af3809f3e2ef0621c6c77b007
Pankajcoder1 <pankajkumarmath1@gmail.com>
2021-08-21 08:58:25 +0530

    firebase app hosted

---
+++ js/change_mode.js
@@ -0,0 +1,147 @@
+var full_screen=document.getElementsByTagName("body");
+
+
+function change_light_mode()
+{
+	// js for main section start
+	full_screen[0].style.backgroundColor="#242526";
+	document.getElementsByClassName("dark")[0].style.display="block";
+	document.getElementsByClassName("light")[0].style.display="none";
+	document.getElementsByClassName("intro")[0].style.color="white";
+	document.getElementsByClassName("intro")[1].style.color="white";
+	document.getElementsByClassName("section-heading")[0].style.borderColor="white";
+	// js section for main section end
+
+	// js for skill section start
+	var list=document.querySelectorAll(".strength_list li p");
+	for(var i=0;i<list.length;i++)
+		list[i].style.color="white";
+	var list=document.querySelectorAll(".programming_list li p");
+	for(var i=0;i<list.length;i++)
+		list[i].style.color="white";
+	var list=document.querySelectorAll(".other_list li p");
+	for(var i=0;i<list.length;i++)
+		list[i].style.color="white";
+
+	// js for skill section end here
+
+	// js for project section start
+	var column=document.querySelectorAll(".table1 tbody tr");
+	for(var i=0;i<column.length;i+=2)
+		column[i].style.backgroundColor="#141414";
+	
+	// js for project section end
+
+	// js for project section start
+	var list=document.querySelectorAll("tbody tr td");
+	for(var i=0;i<list.length;i++)
+		list[i].style.color="white";
+
+	// js for project section end here
+
+	// js for contact me section start
+	var list=document.querySelectorAll(".contact div .row");
+	list[0].style.backgroundColor="#242526";
+
+	// js for contact me section end
+
+	// js for experience section start
+
+	// var logo_background = document.getElementsByClassName("company_logo")[0];
+	// logo_background.style.backgroundColor="white";
+
+	// var column=document.querySelectorAll(".table2 tbody tr");
+	// for(var i=0;i<column.length;i+=2)
+	// 	column[i].style.backgroundColor="#141414";
+
+	// js for experience section end
+
+	// js for connect me section start
+	var github=document.querySelectorAll(".github a i")[0];
+	github.style.color="white";
+	var linkedin=document.querySelectorAll(".linkedin a i")[0];
+	linkedin.style.color="white";
+
+	// js for connect me section end
+
+}
+
+
+function change_dark_mode()
+{
+	// js for main section start
+	full_screen[0].style.backgroundColor="white";
+	document.getElementsByClassName("dark")[0].style.display="none";
+	document.getElementsByClassName("light")[0].style.display="block";
+	document.getElementsByClassName("intro")[0].style.color="white";
+	document.getElementsByClassName("intro")[1].style.color="white";
+	document.getElementsByClassName("section-heading")[0].style.borderColor="white";
+	// js section for main section end
+
+	// js for skill section start
+	var list=document.querySelectorAll(".strength_list li p");
+	for(var i=0;i<list.length;i++)
+		list[i].style.color="black";
+	var list=document.querySelectorAll(".programming_list li p");
+	for(var i=0;i<list.length;i++)
+		list[i].style.color="black";
+	var list=document.querySelectorAll(".other_list li p");
+	for(var i=0;i<list.length;i++)
+		list[i].style.color="black";
+	// js for skill section end here
+
+	// js for project section start
+	var column=document.querySelectorAll(".table1 tbody tr");
+	for(var i=0;i<column.length;i+=2)
+		column[i].style.backgroundColor="rgba(0,0,0,.05)";
+	
+	
+	var list=document.querySelectorAll("tbody tr td");
+	for(var i=0;i<list.length;i++)
+		list[i].style.color="black";
+
+
+	// js for project section end
+	// js for experience section start
+
+	// var logo_background = document.getElementsByClassName("company_logo")[0];
+	// logo_background.style.backgroundColor="transparent";
+
+	// var column=document.querySelectorAll(".table2 tbody tr");
+	// for(var i=0;i<column.length;i+=2)
+	// 	column[i].style.backgroundColor="rgba(0,0,0,0.05)";
+
+	// js for experience section end
+
+	// js for contact me section start
+	var list=document.querySelectorAll(".contact div .row");
+	list[0].style.backgroundColor="white";
+	// js for contact me section end
+
+
+	var github=document.querySelectorAll(".github a i")[0];
+	github.style.color="black";
+	var linkedin=document.querySelectorAll(".linkedin a i")[0];
+	linkedin.style.color="black";
+	$(document).ready(function()
+	{
+		$(github).hover(function()
+		{
+			$(this).css("color","#7456c6");
+		},
+		function()
+		{
+			$(this).css("color","black");
+		});
+		$(linkedin).hover(function()
+		{
+			$(this).css("color","#7456c6");
+		},
+		function()
+		{
+			$(this).css("color","black");
+		});
+	});
+
+	// js for connect me section end
+}
